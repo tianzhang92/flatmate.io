75e4767eb4a35de30b4ba9ec4f2097d0c44cbd60
tianzhang92 <tz2237@columbia.edu>
2015-12-23 11:43:42 -0500

    route test

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,6 @@
 
-var User = require('./../models/User');
-var Message = require('./../models/Message');
+var User = require('/home/bitnami/stack/apps/flatmate.io/app/models/User');
+var Message = require('/home/bitnami/stack/apps/flatmate.io/app/models/Message');
 var craigslist = require('./craigslist');
 var request = require('request');
 var request=require('request');
